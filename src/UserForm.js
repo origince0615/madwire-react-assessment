3f78528b8a2d20c126c103621c5af6026dd1ba75
Eric <nikehero.biz@gmail.com>
2023-04-10 04:30:34 -0400

    complete-userForm

--- src/UserForm.js
+++ src/UserForm.js
@@ -1,20 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const UserForm = () => (
-  <form className="UserForm">
-    <fieldset>
-      <label htmlFor="name">Name</label>
-      <input name="name" id="name"></input>
-    </fieldset>
-    <fieldset>
-      <label htmlFor="username">Username</label>
-      <input name="username" id="username"></input>
-    </fieldset>
-    <fieldset>
-      <label htmlFor="email">Email</label>
-      <input id="email"></input>
-    </fieldset>
-  </form>
-);
+const UserForm = ({ data, onSubmit }) => {
+  const [inputs, setInputs] = useState({
+    name: '',
+    username: '',
+    email: '',
+  });
+
+  useEffect(() => {
+    setInputs(data);
+  }, [data]);
+
+  const handleChange = (e) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+  };
+
+  const onHandleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(inputs);
+  };
+
+  return (
+    <form className="UserForm" onSubmit={onHandleSubmit}>
+      <fieldset>
+        <label htmlFor="name">Name</label>
+        <input
+          name="name"
+          id="name"
+          value={inputs.name}
+          onChange={handleChange}
+        ></input>
+      </fieldset>
+      <fieldset>
+        <label htmlFor="username">Username</label>
+        <input
+          name="username"
+          id="username"
+          value={inputs.username}
+          onChange={handleChange}
+        ></input>
+      </fieldset>
+      <fieldset>
+        <label htmlFor="email">Email</label>
+        <input
+          name="email"
+          id="email"
+          value={inputs.email}
+          onChange={handleChange}
+        ></input>
+      </fieldset>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
 export default UserForm;
